cfdd572dd049bb0542dc1e3240bd20ca038ea7b4
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-03-10 18:33:17 -0600

    fix total code and make run correctly

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,9 +1,8 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-
-
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
       type: String
     },
@@ -12,7 +11,8 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
     },
     size: {
       type: String,
@@ -24,29 +24,26 @@ const PizzaSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Comment'
       }
-    ],
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal)
-    },
+    ]
   },
   {
     toJSON: {
       virtuals: true,
       getters: true
     },
+    // prevents virtuals from creating duplicate of _id as `id`
     id: false
   }
-  );
+);
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
-
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
-
 // export the Pizza model
 module.exports = Pizza;
